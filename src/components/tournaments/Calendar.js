ec7281a917921e2ea111f2c0ad15bbeddf2d370c
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-08-30 15:35:10 +0300

    Work on adding games in subtournaments

--- src/components/tournaments/Calendar.js
+++ src/components/tournaments/Calendar.js
@@ -5,11 +5,17 @@ import compose from "recompose/compose";
 import { FormattedMessage } from "react-intl";
 import { withStyles } from "@material-ui/core/styles";
 
-import { getSeasons } from "../../actions/tournamentActions";
+import { getCommands } from "../../actions/tournamentActions";
 
 import List from "@material-ui/core/List";
 import MenuItem from "@material-ui/core/MenuItem";
+import Avatar from "@material-ui/core/Avatar";
+import InputLabel from "@material-ui/core/InputLabel";
 
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import Button from "@material-ui/core/Button";
 
 const styles = theme => ({
@@ -67,6 +73,16 @@ const styles = theme => ({
   listItem: {
     border: "1px solid rgba(0,0,0,.2)"
   },
+  rounds: {
+    width: 500
+  },
+  roundsBtn: {
+    border: "1px solid #43A047"
+  },
+  selected: {
+    backgroundColor: "#43A047",
+    color: "#fff"
+  },
   success: {
     backgroundColor: "#43A047"
   },
@@ -77,12 +93,37 @@ const styles = theme => ({
 
 class Calendar extends Component {
   state = {
-    tournament: "",
-    seasonsList: null
+    tours: null,
+    tour: "",
+    commands: null,
+    home: "",
+    guest: "",
+    value: 1
+  };
+
+  handleChange = (event, value) => {
+    this.setState({
+      ...this.state,
+      value,
+      tours: Array.from(
+        { length: (this.state.commands.length - 1) * value },
+        (v, k) => k + 1
+      )
+    });
+  };
+
+  onChangeHandler = e => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  };
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+
+    const newGame = {};
   };
 
   componentDidMount() {
-    this.props.getSeasons(this.props.match.url.replace(/\D/g, ""));
+    this.props.getCommands(this.props.match.url.replace(/\D/g, ""));
     this.setState({
       ...this.state,
       tournament: this.props.match.url.replace(/\D/g, "")
@@ -92,10 +133,14 @@ class Calendar extends Component {
   componentWillReceiveProps = nextProps => {
     if (nextProps.errors || nextProps.messages) {
       this.setState({ ...this.state, open: true });
-    } else if (nextProps.tournaments.seasons !== null) {
+    } else if (nextProps.tournaments.commands !== null) {
       this.setState({
         ...this.state,
-        seasonsList: nextProps.tournaments.seasons
+        commands: nextProps.tournaments.commands,
+        tours: Array.from(
+          { length: nextProps.tournaments.commands.length - 1 },
+          (v, k) => k + 1
+        )
       });
     }
   };
@@ -108,7 +153,7 @@ class Calendar extends Component {
           className={classes.button_link}
           to={
             this.state.tournament
-              ? `/seasons/add/${this.state.tournament}`
+              ? `/subtournaments/addgame/${this.state.tournament}`
               : "/"
           }
         >
@@ -116,24 +161,116 @@ class Calendar extends Component {
             <FormattedMessage id="seasons.add" />
           </Button>
         </Link>
-        <List>
-          {this.state.seasonsList !== null
-            ? this.state.seasonsList.map(season => (
-                <Link
-                  className={classes.button_link}
-                  to={`/subtournaments/${season.seaid}`}
-                >
-                  <MenuItem
-                    className={classes.listItem}
-                    key={season.seaid}
-                    value={season.seaid}
-                  >
-                    {season.title}
+        <BottomNavigation
+          value={this.state.value}
+          onChange={this.handleChange}
+          showLabels
+          className={classes.rounds}
+        >
+          <BottomNavigationAction
+            classes={{ selected: classes.selected, root: classes.roundsBtn }}
+            label="1 круг"
+            value={1}
+          />
+          <BottomNavigationAction
+            classes={{ selected: classes.selected, root: classes.roundsBtn }}
+            label="2 круг"
+            value={2}
+          />
+          <BottomNavigationAction
+            classes={{ selected: classes.selected, root: classes.roundsBtn }}
+            label="3 круг"
+            value={3}
+          />
+          <BottomNavigationAction
+            classes={{ selected: classes.selected, root: classes.roundsBtn }}
+            label="4 круг"
+            value={4}
+          />
+        </BottomNavigation>
+        <form onSubmit={this.onSubmitHandler}>
+          {this.state.tours !== null ? (
+            <FormControl className={classes.input}>
+              <InputLabel htmlFor="tour">
+                <FormattedMessage id="subtournaments.statLabel" />
+              </InputLabel>
+              <Select
+                value={this.state.stat}
+                className={classes.select}
+                onChange={this.onChangeHandler}
+                inputProps={{
+                  name: "tour",
+                  id: "tour"
+                }}
+              >
+                <MenuItem value="" />
+                {this.state.tours.map(tour => (
+                  <MenuItem value={tour}>
+                    {tour} <FormattedMessage id="subtournaments.leagueLabel" />
                   </MenuItem>
-                </Link>
-              ))
-            : ""}
-        </List>
+                ))}
+              </Select>
+            </FormControl>
+          ) : (
+            ""
+          )}
+          {this.state.commands !== null ? (
+            <FormControl className={classes.input}>
+              <InputLabel htmlFor="home">
+                <FormattedMessage id="subtournaments.statLabel" />
+              </InputLabel>
+              <Select
+                value={this.state.home}
+                className={classes.select}
+                onChange={this.onChangeHandler}
+                inputProps={{
+                  name: "home",
+                  id: "home"
+                }}
+              >
+                <MenuItem value="" />
+                {this.state.commands.map(command => (
+                  <MenuItem value={command.command_id}>
+                    <Avatar alt="" src={command.logo} />
+                    {command.title}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          ) : (
+            ""
+          )}
+          {this.state.commands !== null ? (
+            <FormControl className={classes.input}>
+              <InputLabel htmlFor="guest">
+                <FormattedMessage id="subtournaments.statLabel" />
+              </InputLabel>
+              <Select
+                value={this.state.guest}
+                className={classes.select}
+                onChange={this.onChangeHandler}
+                inputProps={{
+                  name: "guest",
+                  id: "guest"
+                }}
+              >
+                <MenuItem value="" />
+                {this.state.commands.map(command => (
+                  <MenuItem value={command.command_id}>
+                    <Avatar alt="" src={command.logo} />
+                    {command.title}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          ) : (
+            ""
+          )}
+
+          <Button size="large" type="submit" className={classes.submit}>
+            <FormattedMessage id="subtournaments.submit" />
+          </Button>
+        </form>
       </div>
     );
   }
@@ -149,6 +286,6 @@ export default compose(
   withStyles(styles),
   connect(
     mapStateToProps,
-    { getSeasons }
+    { getCommands }
   )
 )(Calendar);
