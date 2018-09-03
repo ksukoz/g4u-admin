7ab1de51779027853f20582a2bdcf7215f52b33e
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-09-03 17:20:01 +0300

    Add map usin react-geosuggest and react-google-maps

--- src/components/stadiums/AddStadium.js
+++ src/components/stadiums/AddStadium.js
@@ -1,8 +1,12 @@
+/*global google*/
+
 import React, { Component } from "react";
 import { GoogleApiWrapper } from "google-maps-react";
+import Geosuggest from "react-geosuggest";
+
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import compose from "recompose/compose";
+// import compose from "recompose/compose";
 import { FormattedMessage } from "react-intl";
 import { withStyles } from "@material-ui/core/styles";
 
@@ -10,6 +14,37 @@ import List from "@material-ui/core/List";
 import MenuItem from "@material-ui/core/MenuItem";
 
 import Button from "@material-ui/core/Button";
+
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+import { TextField } from "@material-ui/core";
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={props.defaultCenter}
+    onClick={props.onClick}
+  >
+    {props.isMarkerShown && (
+      <Marker position={{ lat: props.lat, lng: props.lng }} />
+    )}
+  </GoogleMap>
+));
 
 const styles = theme => ({
   root: {
@@ -75,16 +110,73 @@ const styles = theme => ({
   mapContainer: {
     width: "100%",
     height: "calc(100vh - (3rem + 64px))",
-    backgroundColor: "#000"
+    "& > div:last-child": {
+      height: "100%"
+    }
   }
 });
 
 class AddStadium extends Component {
+  state = {
+    name: "",
+    defaultCenter: null
+  };
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+  onChangeHandler = e => {
+    this.setState({ ...this.state, name: e.target.value });
+  };
+
+  onSuggestSelect = place => {
+    const { location } = place;
+    this.setState({
+      ...this.state,
+      defaultCenter: {
+        lat: parseFloat(location.lat),
+        lng: parseFloat(location.lng)
+      }
+    });
+  };
+
+  // onClickHandler = e => {
+
+  // }
+
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.mapContainer}>
-        <div />
+        <TextField
+          name="name"
+          value={this.state.name}
+          onChange={this.onChangeHandler}
+        />
+        <Geosuggest
+          placeholder="Start typing!"
+          onSuggestSelect={this.onSuggestSelect}
+          location={
+            this.state.defaultCenter
+              ? this.state.defaultCenter
+              : new google.maps.LatLng(53.558572, 9.9278215)
+          }
+          radius={20}
+        />
+        {this.state.defaultCenter && (
+          <MyMapComponent
+            isMarkerShown
+            defaultCenter={this.state.defaultCenter}
+            lat={this.state.defaultCenter.lat}
+            lng={this.state.defaultCenter.lng}
+            onClick={x => {
+              const lat = x.latLng.lat();
+              const lng = x.latLng.lng();
+
+              this.setState({ ...this.state, defaultCenter: { lat, lng } });
+            }}
+          />
+        )}
       </div>
     );
   }
@@ -97,14 +189,15 @@ const mapStateToProps = state => ({
 
 export default compose(
   withStyles(styles),
-  GoogleApiWrapper(
-    props =>
-      props.location.state
-        ? {
-            apiKey: props.location.state.apiKey
-          }
-        : ""
-  ),
+  // GoogleApiWrapper(
+  //   props =>
+  //     props.location.state
+  //       ? {
+  //           apiKey: props.location.state.apiKey,
+  //           libraries: ["places"]
+  //         }
+  //       : ""
+  // ),
   connect(
     mapStateToProps,
     null
